46bd815bfa8cb27c2f92930c2ad4cc1a8e46e192
KostiantynFrolov <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>
2024-05-04 21:02:44 +0200

    Import templates, static files, make base template, change imported templates, bind static files with html, create four views

---
+++ app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import LandingPageView, AddDonationView, LoginView, RegisterView
+
+urlpatterns = [
+    path("", LandingPageView.as_view(), name="landing_page"),
+    path("add-donation/", AddDonationView.as_view(), name="add_donation"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("register/", RegisterView.as_view(), name="register")
+]
