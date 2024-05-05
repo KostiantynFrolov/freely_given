9307265bf7782ffef3d67044a4ae8249ebf740ae
KostiantynFrolov <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>
2024-05-05 18:43:00 +0200

    Bind numbers of donated bags and institutions from database with landing page

--- app/views.py
+++ app/views.py
@@ -1,10 +1,15 @@
+from django.db.models import Sum
 from django.shortcuts import render
 from django.views import View
+from .models import Donation
 
 
 class LandingPageView(View):
     def get(self, request):
-        return render(request, "index.html")
+        donated_bags_sum = Donation.objects.aggregate(Sum("quantity"))["quantity__sum"]
+        donated_institutions_sum = len(set(Donation.objects.values_list("institution", flat=True)))
+        return render(request, "index.html", {"donated_bags_sum": donated_bags_sum,
+                                              "donated_institutions_sum": donated_institutions_sum})
 
 
 class AddDonationView(View):
