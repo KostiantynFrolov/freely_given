a488c5f544fb0f335e7273d546d9421557d5260d
KostiantynFrolov <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>
2024-05-07 17:32:26 +0200

    Show information about foundations, non-government organizations and local collections from database on the homepage

--- app/models.py
+++ app/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
+
 
 class Institution(models.Model):
     FOUNDATION = "f"
--- app/views.py
+++ app/views.py
@@ -1,15 +1,21 @@
 from django.db.models import Sum
 from django.shortcuts import render
 from django.views import View
-from .models import Donation
+from .models import Donation, Institution
 
 
 class LandingPageView(View):
     def get(self, request):
         donated_bags_sum = Donation.objects.aggregate(Sum("quantity"))["quantity__sum"]
         donated_institutions_sum = len(set(Donation.objects.values_list("institution", flat=True)))
+        foundations_all = Institution.objects.filter(type="f")
+        non_gov_organizations_all = Institution.objects.filter(type="ngo")
+        local_collections_all = Institution.objects.filter(type="lc")
         return render(request, "index.html", {"donated_bags_sum": donated_bags_sum,
-                                              "donated_institutions_sum": donated_institutions_sum})
+                                              "donated_institutions_sum": donated_institutions_sum,
+                                              "foundations_all": foundations_all,
+                                              "non_gov_organizations_all": non_gov_organizations_all,
+                                              "local_collections_all": local_collections_all})
 
 
 class AddDonationView(View):
