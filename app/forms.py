b0890ef42dc51bb17f15a47a56079d41c56a8745
KostiantynFrolov <144308626+KostiantynFrolov@users.noreply.github.com>
2024-06-07 10:07:29 +0200

    Add additional password validations

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,9 @@
+import string
+
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class RegisterForm(UserCreationForm):
@@ -17,6 +20,18 @@ class RegisterForm(UserCreationForm):
         self.fields["username"].widget.attrs["placeholder"] = "Email"
         self.fields["password1"].widget.attrs["placeholder"] = "Hasło"
         self.fields["password2"].widget.attrs["placeholder"] = "Powtórz hasło"
+
+    def clean_password1(self):
+        password1 = self.cleaned_data.get("password1")
+        if not any(symbol.isupper() for symbol in password1):
+            raise ValidationError("To hasło musi zawierać co najmniej jedną wielką literę.")
+        if not any(char.islower() for char in password1):
+            raise ValidationError("To hasło musi zawierać co najmniej jedną małą literę.")
+        if not any(elem.isdigit() for elem in password1):
+            raise ValidationError("To hasło ma zawierać co najmniej jedną cyfrę.")
+        if not any(item in string.punctuation for item in password1):
+            raise ValidationError("To haso ma zawierać co najmniej jeden znak specjalny.")
+        return password1
 
 
 class PasswordConfirmForm(forms.Form):
