78361309b78c39c7c0f68871fd8c746003a8feab
KostiantynFrolov <144308626+KostiantynFrolov@users.noreply.github.com>
2024-07-17 19:35:58 +0200

    Final version

--- app/views.py
+++ app/views.py
@@ -5,9 +5,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.auth.views import LogoutView, PasswordChangeView
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.core.paginator import Paginator
 from django.db.models import Sum
-from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.utils import translation
